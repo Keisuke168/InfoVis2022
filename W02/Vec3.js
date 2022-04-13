d9d0501ac5a816c27cd212b52152ce9dab55d852
Keisuke168 <k.yoshimi313@ezweb.ne.jp>
2022-04-13 14:45:48 +0900

    task2

--- W02/Vec3.js
+++ W02/Vec3.js
@@ -1,18 +1,64 @@
-class Vec3
-{
-    constructor(x, y, z)
-    {
+class Vec3{
+    constructor(x, y, z){
         this.x = x;
         this.y = y;
         this.z = z;
     }
 
-    add( v )
-    {
+    add( v ){
         this.x += v.x;
         this.y += v.y;
         this.z += v.z;
         return this;
+    }
+
+    min(){
+        var minimum = this.x
+        if (this.y < minimum){
+            var minimum = this.y;
+        }
+        if (this.z < minimum){
+            var minimum = this.z;
+        }
+        return minimum;
+    }
+
+    mid(){
+        if(this.x < this.y){
+            if(this.x > this.z){
+                return this.x;
+            }
+            else if(this.y < this.z){
+                return this.y;
+            }
+            else{
+                return this.z;
+            }
+        }
+        else{
+            if(this.y > this.z){
+                return this.y
+            }
+            else if(this.x < this.z){
+                return this.x;
+            }
+            else{
+                return this.z;
+            }
+            
+        }
+    }
+
+
+    max(){
+        var maximum = this.x
+        if (this.y > maximum){
+            maximum = this.y;
+        }
+        if (this.z > maximum){
+            maximum = this.z;
+        }
+        return maximum;
     }
     
 }
